8c3f9b5cf7e3d4c2abca2e511614949a0d3cebca
Lodek <23224552+Lodek@users.noreply.github.com>
2018-12-11 14:50:54 -0200

    terminal and circuit classes

--- pdd/digital_logic.py
+++ pdd/digital_logic.py
@@ -30,6 +30,7 @@ their signals are equal"""
     def __init__(self, n=1, signal=0):
         self.wires = [Wire() for _ in range(n)]
         self.signal = signal
+        logger.debug("New {}".format(repr(self)))
 
     def __repr__(self):
         return 'Bus({}, n={})'.format(self.signal, len(self))
@@ -54,7 +55,7 @@ their signals are equal"""
         return sig
 
     def extend(self, size):
-        """Changes size of self to size, new Wires are initialized to 0"""
+        """Changes the number of wires in the bus, new Wires are initialized to 0"""
         nsize = size - len(self)
         if nsize > 0:
             self.wires += [Wire() for _ in range(nsize)]
@@ -63,12 +64,12 @@ their signals are equal"""
 
     @property
     def signal(self):
-        """Computes Signal for bus and returns it"""
+        """Returns Signal object for bus"""
         return Signal(int(self), len(self))
         
     @signal.setter
     def signal(self, value):
-        """Constructs a Signal from value and assigns it to self"""
+        """ Assigns a new Signal to the bus and notifies the updater"""
         bits = Signal(value, len(self)).to_bits()
         for wire, bit in zip(self.wires, bits):
             wire.bit = bit
@@ -81,3 +82,42 @@ their signals are equal"""
         bus.wires = wires
         return bus
  
+VDD = Bus(1, 1)
+GND = Bus()
+    
+class Terminal:
+
+    """Terminals makes the transition between inside and outside a circuit.
+Terminals are conceptually similar to buffers, its input and output are connected
+to distinct buses. The terminal reads the input bus' signal and writes 
+that same signal to the output bus. It's possible to use a Terminal as a NOT
+gate as well, in which case the output would be the logical negation of the input.
+The input and output for every circuit Block is a Terminal.
+Optionally, it is possible to toggle whether the output propagates the signal
+to the bus or not. If self.connected is tied to GND, then the signal will not propagate
+this is analogous to being at a High Impedance."""
+
+    def __init__(self, bus_rule=0, connected=VDD, invert=False):
+        self._in_bus = None
+        self.out_bus = Bus()
+        self.bus_rule = bus_rule
+        self.invert = invert
+        self.connected = connected
+
+    @property
+    def in_bus(self):
+        return self._in_bus
+
+    @in_bus.setter
+    def in_bus(self, bus):
+        if len(bus) != self.bus_rule and bus_rule != 0:
+            #BusError class?
+            raise ValueError("Bus size incompatible with requirements")
+        self._in_bus = bus
+        l = len(bus)
+        self.out_bus.extend(l)
+        
+    def propagate(self):
+        in_sig = self.in_bus.signal
+        if self.connected == VDD:
+            self.out_bus.signal = in_sig if not self.invert else in_sig.complement()
