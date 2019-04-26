b86c2a9ebf6616320ac3a57dd65805368425a680
Lodek <23224552+Lodek@users.noreply.github.com>
2019-04-26 19:54:14 -0300

    setup commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='pdd',
+    version='0.0.1',
+    author='Lodek',
+    description='Pytho Digital Design',
+    packages=setuptools.find_packages())
+
+
+    
