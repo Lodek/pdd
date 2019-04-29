467f3a291e882faeae9ef20289d4990188797429
Lodek <23224552+Lodek@users.noreply.github.com>
2019-04-29 19:50:01 -0300

    setup update

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 setuptools.setup(
     name='pdd',
     version='0.0.1',
-    author='Lodek',
-    description='Pytho Digital Design',
+    author='Bruno Gomes',
+    description='Python Digital Design',
     packages=setuptools.find_packages())
 
 
